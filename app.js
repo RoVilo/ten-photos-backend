7fba9073ca090b7be869fd34a9242d871c70f06d
Roman Sladecek <sladecek.roman@gmail.com>
2017-06-20 21:42:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+app.get('/users', function(req, res) {
+    console.log('Requested users');
+    res.send('List of users');
+})
+
+app.get('/users/:userid', function(req, res) {
+    console.log('Requested users');
+    res.send('Deatail of user ' + req.params.userid);
+})
