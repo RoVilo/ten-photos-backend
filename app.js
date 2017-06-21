f72f883ccb34914178008991de43d3dcde28565b
Roman Sladecek <sladecek.roman@gmail.com>
2017-06-21 10:48:58 +0200

    [*] blaaaa

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Tu sa bude tvorit mega projeeekt')
+  res.send('Tu sa bude tvorit mega projeeekt 222')
 })
 
 // var port = process.env.port || 3000;
