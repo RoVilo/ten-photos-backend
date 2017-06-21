bbfd304b8bfc1e1d1f47edb1be0739f1b1425b55
Roman Sladecek <sladecek.roman@gmail.com>
2017-06-21 14:48:38 +0200

    [*] image upload

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require('express')
 var multer  = require('multer')
 var upload = multer({ dest: 'public/uploads/' })
 const app = express()
+var path = require('path');
+var serveIndex = require('serve-index')
 
 
-app.get('/', function (req, res) {
-  res.send('Tu sa bude tvorit mega projeeekt 222')
+
+// app.get('/', function (req, res) {
+//   res.send('Tu sa bude tvorit mega projeeekt 222')
+// })
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/', upload.any(), function (req, res, next) {
+  res.send(req.files)
 })
+
+// Serve URLs like /ftp/thing as public/ftp/thing
+app.use('/files', serveIndex('public/uploads', {'icons': true}))
 
 // var port = process.env.port || 3000;
 
@@ -14,16 +28,14 @@ app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-app.get('/users', function(req, res) {
-    console.log('Requested users');
-    res.send('List of users');
-})
+// app.get('/users', function(req, res) {
+//     console.log('Requested users');
+//     res.send('List of users');
+// })
 
-app.get('/users/:userid', function(req, res) {
-    console.log('Requested users');
-    res.send('Deatail of user ' + req.params.userid);
-})
+// app.get('/users/:userid', function(req, res) {
+//     console.log('Requested users');
+//     res.send('Deatail of user ' + req.params.userid);
+// })
 
-app.post('/', upload.any(), function (req, res, next) {
-  res.send(req.files)
-})
+
